6d5f7cf4651339623ecfd2fce38e4ec3bb7d7a83
RinSer <serj.dukareff@gmail.com>
2015-11-26 15:12:36 +0300

    Second day ImgCache

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -16,11 +16,6 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Secure file formats
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
-
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
@@ -43,14 +38,19 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
   # end
+
+  # Cache for Heroku
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
 
 end
