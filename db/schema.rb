42496123187005157a31ace231b54e25c51efc03
RinSer <serj.dukareff@gmail.com>
2015-11-30 11:57:08 +0300

    Add pic_url

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123124500) do
+ActiveRecord::Schema.define(version: 20151130083804) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20151123124500) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.string   "pic_url"
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id"
